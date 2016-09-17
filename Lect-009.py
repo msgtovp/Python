658ef3dcb952dcc465800fbf0bb71c3699528343
msgtovp <msgtovp@gmail.com>
2016-09-17 14:32:02 +0530

    Add files via upload

---
+++ Lect-009.py
@@ -0,0 +1,9 @@
+name = 'Steve'
+print (len(name))
+print (name[0])
+print (name[1])
+print (name[4])
+print ('Negative Starts')
+print (name[-2])
+print (name[-3])
+print (name[-5])
---
+++ Lect-010.py
@@ -0,0 +1,6 @@
+name = 'madams'
+print ("Guido Van Rossom"[6:10])
+if name == name[::-1]:
+	print ('Palindrome')
+else:
+	print ('Not Palindrome')
